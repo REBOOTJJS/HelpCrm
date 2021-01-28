70caa108ffb5a961ef487378aacc827e5703cfa6
Joaquin Jimenez <joaquinjimenezmail@gmail.com>
2021-01-28 23:58:40 +0100

    Funciona hasta listado de cuentas con concatenacion de campos

--- public/js/home.js
+++ public/js/home.js
@@ -1,18 +1,23 @@
-  //pq npm comunica front con backend postman del frontend **
-axios
-  .get('http://localhost:3000/api/accounts', { headers: { token: localStorage.getItem('token')}})
-  .then(response => {
-    const cuentas = document.getElementById('cuentas');
-    response.data.forEach(cuenta => {
-      const newCuenta = document.createElement('li')
-      newCuenta.innerHTML = cuenta.nombre;
-      cuentas.appendChild(newCuenta)
+ //pq npm comunica front con backend postman del frontend **
+ console.log(localStorage.getItem('token'))
+ axios
+ .get('http://localhost:3000/api/accounts', { headers: { token: localStorage.getItem('token')}})
+ .then(response => {
+  console.log(response.data) // para ver lo que llega
+  const cuentas = document.getElementById('cuentas');
+   response.data.forEach(cuenta => {
+     const newCuenta = document.createElement('li')
+     newCuenta.innerHTML = `${cuenta.nombre} - ${cuenta.categoria} - ${cuenta.telefono}` ;
+     cuentas.appendChild(newCuenta)
+ })
 
+})
+  .catch(error => {
+    console.log();
   })
+
 //ver **
-  document.getElementById('logout').addEventListener("click", function() {
-    localStorage.clear();
-    window.location.reload()
-  })
-
-
+ document.getElementById('logout').addEventListener("click", function() {
+   localStorage.clear();
+   window.location.reload()
+ })
